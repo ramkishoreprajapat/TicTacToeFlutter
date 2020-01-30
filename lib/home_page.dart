833ec9d008bab88e130e95c6388e6642debcefaf
unknown <shekhar.prajapat02@gmail.com>
2020-01-30 23:31:26 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,246 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/custom_dialog.dart';
+import 'package:tic_tac_toe/game_button.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<GameButton> buttonList;
+  var player1;
+  var player2;
+  var activePlayer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    buttonList = doInit();
+  }
+
+  List<GameButton> doInit() {
+    player1 = new List();
+    player2 = new List();
+    activePlayer = 1;
+
+    var gameButtons = <GameButton>[
+      GameButton(id: 1),
+      GameButton(id: 2),
+      GameButton(id: 3),
+      GameButton(id: 4),
+      GameButton(id: 5),
+      GameButton(id: 6),
+      GameButton(id: 7),
+      GameButton(id: 8),
+      GameButton(id: 9),
+    ];
+
+    return gameButtons;
+  }
+
+  playGame(GameButton gb) {
+    setState(() {
+      if (activePlayer == 1) {
+        gb.text = "X";
+        gb.bg = Colors.blue;
+        activePlayer = 2;
+        player1.add(gb.id);
+      } else {
+        gb.text = "0";
+        gb.bg = Colors.black;
+        activePlayer = 1;
+        player2.add(gb.id);
+      }
+
+      gb.enabled = false;
+      int winner = checkWinner();
+
+      if (winner == -1) {
+        if (buttonList.every((test) => test.text != "")) {
+          showDialog(
+              context: context,
+              builder: (_) => CustomDialog("Game Tied",
+                  "Press the reset button to start again.", resetGame));
+        } else {
+          activePlayer == 2 ? autoPlay() : null;
+        }
+      }
+    });
+  }
+
+  int checkWinner() {
+    var winner = -1;
+    //row 1
+    if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+      winner = 1;
+    }
+
+    if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+      winner = 2;
+    }
+
+    //row2
+    if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+      winner = 1;
+    }
+
+    if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+      winner = 2;
+    }
+
+    //row3
+    if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+      winner = 1;
+    }
+
+    if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    //col1
+    if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+      winner = 1;
+    }
+
+    if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    //col2
+    if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+      winner = 1;
+    }
+
+    if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+      winner = 2;
+    }
+
+    //col3
+    if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+      winner = 1;
+    }
+
+    if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    //diagonal
+    if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+      winner = 1;
+    }
+
+    if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+      winner = 1;
+    }
+
+    if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    if (winner != -1) {
+      if (winner == 1) {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player 1 Won",
+                "Press the reset button to start again.", resetGame));
+      } else {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player 2 Won",
+                "Press the reset button to start again.", resetGame));
+      }
+    }
+
+    return winner;
+  }
+
+  void resetGame() {
+    if (Navigator.canPop(context)) Navigator.pop(context);
+    setState(() {
+      buttonList = doInit();
+    });
+  }
+
+  void autoPlay() {
+    var emptyCells = List();
+    var list = List.generate(9, (i) => i + 1);
+    for (var cellID in list) {
+      if (!(player1.contains(cellID) || player2.contains(cellID))) {
+        emptyCells.add(cellID);
+      }
+    }
+
+    var r = Random();
+    var randIndex = r.nextInt(emptyCells.length - 1);
+    var cellID = emptyCells[randIndex];
+    int i = buttonList.indexWhere((p) => p.id == cellID);
+    playGame(buttonList[i]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tic Tac Toe"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(16),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 9.0,
+                  mainAxisSpacing: 9.0,),
+              itemCount: buttonList.length,
+              itemBuilder: (context, i) => SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: new Container(
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10)),
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: buttonList[i].enabled
+                        ? () => playGame(buttonList[i])
+                        : null,
+                    child: Text(
+                      buttonList[i].text,
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    color: buttonList[i].bg,
+                    disabledColor: buttonList[i].bg,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          new Container(
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(16),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+            child: new InkWell(
+              onTap: resetGame,
+              child: Text(
+                "Reset",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
